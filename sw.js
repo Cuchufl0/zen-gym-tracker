153eebe56dd611addf80abd611157c54d0d25167
Cuchufl0 <lucasgarrahan@gmail.com>
2026-05-08 22:54:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,36 @@
-const CACHE_NAME = 'grimoire-final-v1';
+const CACHE_NAME = 'grimoire-v6';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
+  self.skipWaiting(); 
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.map((key) => caches.delete(key))
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); 
+          }
+        })
       );
-    }).then(() => self.clients.claim())
+    }).then(() => self.clients.claim()) 
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  // Network-First Strategy: Always grabs the newest updates instantly
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
