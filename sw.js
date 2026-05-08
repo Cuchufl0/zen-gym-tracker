589f3a44e8a1073e6f426907385b264315c9a9d1
Cuchufl0 <lucasgarrahan@gmail.com>
2026-05-08 23:21:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,20 @@
-const CACHE_NAME = 'grimoire-v6';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'zen-master-v1';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting(); 
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key); 
-          }
-        })
+        keys.map((key) => caches.delete(key))
       );
     }).then(() => self.clients.claim()) 
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  // Network-First Strategy: Always grabs the newest updates instantly
   event.respondWith(
     fetch(event.request).catch(() => {
       return caches.match(event.request);
